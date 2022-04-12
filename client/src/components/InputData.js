b5ca6c97bea832e4311ca5055fde1548497781d0
Dane <danejwhitfield@gmail.com>
2022-04-12 09:02:54 +0100

    styling updates

--- client/src/components/InputData.js
+++ client/src/components/InputData.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import { fetchFunc } from "../utils/fetchFunc";
+import { FaSearch } from "react-icons/fa";
 
 export default function InputData({ setResultData }) {
   const [formData, setFormData] = useState("");
 
   return (
     <div>
-      <h1>Input Data</h1>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          value={formData}
-          onChange={(e) => setFormData(e.target.value)}
-        />
-        <button
-          onClick={() => {
-            fetchFunc(formData).then((data) => {
-              console.log("data:", data);
-              setResultData(data);
-            });
-          }}
-        >
-          CLICK ME
-        </button>
+      {/* <h1>Enter your postcode</h1> */}
+      <form className="form" onSubmit={(e) => e.preventDefault()}>
+        <div className="input-container">
+          <input
+            placeholder="M26, BD2 etc."
+            type="text"
+            value={formData}
+            onChange={(e) => setFormData(e.target.value)}
+          />
+          <button
+            className="submit-btn"
+            onClick={() => {
+              fetchFunc(formData).then((data) => {
+                console.log("data:", data);
+                setResultData(data);
+              });
+            }}
+          >
+            <span className="search-icon">
+              <FaSearch />
+            </span>
+          </button>
+        </div>
       </form>
     </div>
   );
